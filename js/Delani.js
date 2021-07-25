a1722432ceec5ba460a58716d785f44d125cb3c1
MosesGakuhi1857 <king'orimoses386@gmail.com>
2021-07-25 03:31:57 +0300

    added behaviour to the portfolio section

--- js/Delani.js
+++ js/Delani.js
@@ -1,16 +1,94 @@
-
 // start of jquery/
 
+$(document).ready(function () {
+    $("#image-content1").click(function (){
+        $("#content1").toggle();
+        $("#image-content1").toggle();
+    });
+    $("#content1").click(function () {
+        $("#image-content1").toggle();
+        $("#content1").hide();
+    });
+    
+    //id 2
+    $("#image-content2").click(function () {
+        $("#content2").toggle();
+        $("#image-content2").toggle();
+    });
+    $("#content2").click(function () {
+        $("#image-content2").toggle();
+        $("#content2").hide();
+    });
+    //id 3
+    $("#image-content3").click(function () {
+        $("#content3").toggle();
+        $("#image-content3").toggle();
+    });
+    $("#content3").click(function () {
+        $("#image-content3").toggle();
+        $("#content3").hide();
+    });;
+
+});
+  
+
+// portfolio
 $(document).ready(function(){
-$(".image-content").click(function(){
-$(".content").toggle()
-$(".image-content").toggle()
-})
-$(".content").click(function(){
-    $(".image-content").toggle()
-     $(".content").hide()
-})
+$("#one").hover(function(){
+     $("#text1").animate({opacity:1},60)}, //mouse over
 
-
-
+     function(){
+     $("#text1").animate({opacity:0},50)  //mouse out
+     })
+//two
+     $("#two").hover(function(){
+        $("#text2").animate({opacity:1},60)},
+   
+        function(){
+        $("#text2").animate({opacity:0},50)
+        })
+//three
+        $("#three").hover(function(){
+            $("#text3").animate({opacity:1},60)},
+       
+            function(){
+            $("#text3").animate({opacity:0},50)
+            })
+//four
+            $("#four").hover(function(){
+                $("#text4").animate({opacity:1},60)},
+           
+                function(){
+                $("#text4").animate({opacity:0},50)
+                })
+//five
+                $("#five").hover(function(){
+                    $("#text5").animate({opacity:1},60)},
+               
+                    function(){
+                    $("#text5").animate({opacity:0},50)
+                    })
+//six
+                    $("#six").hover(function(){
+                        $("#text6").animate({opacity:1},60)},
+                   
+                        function(){
+                        $("#text6").animate({opacity:0},50)
+                        })
+//seven
+                        $("#seven").hover(function(){
+                            $("#text7").animate({opacity:1},60)},
+                       
+                            function(){
+                            $("#text7").animate({opacity:0},50)
+                            })
+//eight
+                            $("#eight").hover(function(){
+                                $("#text8").animate({opacity:1},60)},
+                           
+                                function(){
+                                $("#text8").animate({opacity:0},50)
+                                });
+        
+    
 })
